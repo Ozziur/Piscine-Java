263f12975881916c8c6ab3c6bbe45c9a7e72b828
mruizzo <casaruizzo@yahoo.it>
2023-04-06 14:56:17 +0200

    teoricamente chiuso ex01

--- Day 00/ex01/Program.java
+++ Day 00/ex01/Program.java
@@ -4,26 +4,38 @@ public class Program
 {
 	public static void prime(int number) {
 
-		System.out.println("You entered: " + number);
+		int i = 0;
+		int mov = 1;
+		boolean p_answer= true;
+
+		for (i = 2; number >= i * i; i++){
+			if (number % i == 0){
+				p_answer = false;
+				break ;
+				}
+			mov++;
+		}
+		if (i == number)
+			p_answer= true;
+		System.out.println(p_answer+ "\t" + mov);
 	}
 
 	public static void main(String[] x){
 
 		Scanner scanner = new Scanner(System.in);
 		int number = 0;
-		try {
-			System.out.print("Enter an integer: ");
+		try{
 			number = scanner.nextInt();
 			if (number <= 1){
-				System.err.println("Input not valid.");
+				System.err.println("IllegalArgument");
+				System.exit(-1);
 			} else{
 				prime(number);
-			}
-			
+				System.exit(0);
+			}			
 		} catch (Exception e) {
-			System.err.println("Input not valid.");	
+			System.err.println("IllegalArgument");
+			System.exit(-1);
 		}
-
-       //System.out.println("You entered: " + number);
 	}
 }
